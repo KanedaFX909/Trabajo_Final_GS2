a4ef1fea50d7bb1a599b6625d253e88a0eaa8641
Mat <matias.obrist93@gmail.com>
2022-10-25 20:26:40 -0300

    Primer commit, secuencia logica el programa version 1.1

--- Autos.py
+++ Autos.py
@@ -1,6 +1,6 @@
 import datetime
 
-print('Sistema de Gestión QK')
+print('Sistema de Gestión Autos')
 class Auto:
     def __init__(self):
         self.marca = ''
@@ -10,6 +10,7 @@ class Auto:
         self.km = 0
         self.fecha_creacion = datetime.date.today()
     
+    #Función agregar autos, solicita inputs, solo se aceptan nros en AÑO Y KM
     def agregar_auto(self):
         try:
             self.marca = input('Marca: ')
@@ -23,19 +24,23 @@ class Auto:
             return False
 
     def __str__(self):
+        #esta funcion convierte en str todos los elementos de la lista
         return '\t'.join(str(x) for x in [self.marca, self.model, self.color, self.año, self.km, self.fecha_creacion ])
 
 class Inventario:
+    #Guardo los autos en esta lista vacia
     def __init__(self):
         self.vehiculos = []
-        
+    
+    #Llamo a la funcion agregar auto, ingresa a la lista y emite mensaje
     def agregar_auto(self):
         vehiculo = Auto()
         if vehiculo.agregar_auto() == True:
             self.vehiculos.append(vehiculo)
             print ()
             print("El auto se agrego a la lista")
-            
+    
+    #Funcion para visualizar el inventario       
     def viewInventory(self):
         print('\t'.join(['Nro', 'Marca', 'Modelo','Color', 'Año', 'km','Fecha']))
         for i, vehiculo in enumerate(self.vehiculos) :
