b64c87a53d6287c856f0c9b164ce6ed6b22a9b69
Matias.Obrist <63169360+KanedaFX909@users.noreply.github.com>
2022-11-02 23:36:51 -0300

    Nueva version car.py, cada property con su setter
    -

--- Car.py
+++ Car.py
@@ -2,6 +2,7 @@ from datetime import date
 
 class Car:
     def __init__(self, brand, model, color, year, km):
+        #Atributos inicializados en modo privado
         self.__brand = brand
         self.__model = model
         self.__color = color
@@ -9,30 +10,46 @@ class Car:
         self.__km = km
         self.__date_creation = date.today()
 
-    @property
+    @property # Acceso a la variable por medio del decorador @property
     def brand(self):
         return self.__brand
+    @brand.setter
+    def brand(self, brand):
+        self.__brand = brand
+
 
     @property
     def model(self):
         return self.__model
+    @model.setter
+    def model(self, model):
+        self.__model = model
+
 
     @property
     def color(self):
         return self.__color
+    @color.setter
+    def color(self, color):
+        self.__color = color
+
 
     @property
     def year(self):
         return self.__year
+    @year.setter
+    def year(self, year):
+        self.__year = year
+
 
     @property
     def km(self):
         return self.__km
-
     @property
     def date_creation(self):
         return self.__date_creation
 
+    # Setter decorado c/ atributo de acceso global
     @date_creation.setter
     def date_creation(self, date_creation):
         self.__date_creation = date_creation
